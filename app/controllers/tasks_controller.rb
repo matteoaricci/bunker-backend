bd95a26d6c5ebae52c5368e8121aeb860a5b5ea4
matteoaricci <matteo.a.ricci@gmail.com>
2021-02-12 16:09:19 -0500

    can create new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,7 @@
 class TasksController < ApplicationController
+
+    def create 
+        task = Task.create(column_id: params[:column_id], description: params[:description])
+        render json: task
+    end
 end
